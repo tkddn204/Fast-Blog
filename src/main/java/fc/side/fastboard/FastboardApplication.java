4f16f32d8629bb99cee41f15c558319b762133af
tjdtn0219 <s_sjang@naver.com>
2023-08-31 20:01:46 +0900

    Init:Project

---
+++ src/main/java/fc/side/fastboard/FastboardApplication.java
@@ -0,0 +1,13 @@
+package fc.side.fastboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fc/side/fastboard/FastboardApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.side.fastboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
