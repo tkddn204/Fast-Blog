0edfbc845990079d2385bd110b1cebf736172d4f
tjdtn0219 <s_sjang@naver.com>
2023-09-15 20:05:28 +0900

    게시글 목록 페이징 구현

--- src/main/java/fc/side/fastboard/board/controller/BoardController.java
+++ src/main/java/fc/side/fastboard/board/controller/BoardController.java
@@ -7,6 +7,10 @@ import fc.side.fastboard.board.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +24,11 @@ public class BoardController {
 
   private final BoardService boardService;
 
-  @GetMapping("")
-  public String index() {
+  @GetMapping("/")
+  public String index(Model model) {
+    List<BoardDetailDTO> boards = boardService.getAllBoards();
+    log.info("getBoards={}", boards);
+    model.addAttribute("boards", boards);
     return "index";
   }
 
@@ -49,9 +56,20 @@ public class BoardController {
   }
 
   @GetMapping("/boards")
-  public String test(Model model) {
-    List<BoardDetailDTO> boards = boardService.getAllBoards();
+  public String getMyBoards(
+          Model model,
+          @PageableDefault(page=0, size=3, sort="id", direction = Sort.Direction.DESC)
+          Pageable pageable
+  ) {
+    Page<BoardDetailDTO> boards = boardService.getMyBoards(pageable);
+    int nowPage = boards.getPageable().getPageNumber() + 1;
+    log.info("nowPage={}", nowPage);
+    int startPage = Math.max(nowPage-3, 1);
+    int endPage = Math.min(nowPage + 4, boards.getTotalPages());
     model.addAttribute("boards", boards);
+    model.addAttribute("nowPage", nowPage);
+    model.addAttribute("startPage", startPage);
+    model.addAttribute("endPage", endPage);
     return "board/listForm";
   }
 }
