16889cd45aec079ca67e8e56c1706b7a8e54edf1
tjdtn0219 <s_sjang@naver.com>
2023-09-15 20:36:29 +0900

    Refactor : Model에 Page설정하는 메소드 분리 및 페이징 길이 환경변수 분리

--- src/main/java/fc/side/fastboard/board/controller/BoardController.java
+++ src/main/java/fc/side/fastboard/board/controller/BoardController.java
@@ -4,16 +4,19 @@ import fc.side.fastboard.board.dto.BoardDetailDTO;
 import fc.side.fastboard.board.dto.CreateBoardDTO;
 import fc.side.fastboard.board.entity.Board;
 import fc.side.fastboard.board.service.BoardService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,6 +26,8 @@ import java.util.List;
 public class BoardController {
 
   private final BoardService boardService;
+  @Value("${pagination.width}")
+  private int width;
 
   @GetMapping("/")
   public String index(Model model) {
@@ -58,18 +63,25 @@ public class BoardController {
   @GetMapping("/boards")
   public String getMyBoards(
           Model model,
-          @PageableDefault(page=0, size=3, sort="id", direction = Sort.Direction.DESC)
+          @PageableDefault(size=3, sort="id", direction = Sort.Direction.DESC)
           Pageable pageable
   ) {
     Page<BoardDetailDTO> boards = boardService.getMyBoards(pageable);
-    int nowPage = boards.getPageable().getPageNumber() + 1;
-    log.info("nowPage={}", nowPage);
-    int startPage = Math.max(nowPage-3, 1);
-    int endPage = Math.min(nowPage + 4, boards.getTotalPages());
     model.addAttribute("boards", boards);
+    setPagingModel(model, pageable.getPageNumber(), boards.getTotalPages());
+    return "board/listForm";
+  }
+
+  private void setPagingModel(Model model, int pageNum, int totalPages) {
+    log.info("width={}", width);
+
+    int nowPage = pageNum + 1;
+    int startPage = Math.max(nowPage-3, 1);
+    int endPage = Math.min(nowPage + 4, totalPages);
     model.addAttribute("nowPage", nowPage);
     model.addAttribute("startPage", startPage);
     model.addAttribute("endPage", endPage);
-    return "board/listForm";
   }
+
+
 }
