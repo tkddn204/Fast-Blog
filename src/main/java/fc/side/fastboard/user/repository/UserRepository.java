55c14f79739dd50361cc535fc02405c16fbce6db
jun <jundev21@gmail.com>
2023-09-17 19:03:09 +0900

    [Update] 레포지토리 어노테이션 추가

--- src/main/java/fc/side/fastboard/user/repository/UserRepository.java
+++ src/main/java/fc/side/fastboard/user/repository/UserRepository.java
@@ -2,8 +2,11 @@ package fc.side.fastboard.user.repository;
 
 import fc.side.fastboard.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+@Repository
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
