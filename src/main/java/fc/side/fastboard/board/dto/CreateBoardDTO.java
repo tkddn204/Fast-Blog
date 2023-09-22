1be1a9c36dc128b6534e71b57b68b8821893b944
tjdtn0219 <s_sjang@naver.com>
2023-09-22 17:10:45 +0900

    게시글 수정/작성  Validation 검증 로직  추가

--- src/main/java/fc/side/fastboard/board/dto/CreateBoardDTO.java
+++ src/main/java/fc/side/fastboard/board/dto/CreateBoardDTO.java
@@ -3,6 +3,7 @@ package fc.side.fastboard.board.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import fc.side.fastboard.board.entity.Board;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -19,12 +20,14 @@ import java.time.LocalDateTime;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateBoardDTO {
 
-    @NotNull
+//    @NotNull
     private Integer writerId;
 
-    @Size(min = 3, max = 15, message = "title size must be 3-15.")
+    @NotBlank
+    @Size(min = 3, max = 15)
     private String title;
 
+    @NotBlank
     private String content;
 
     public static Board toEntity(CreateBoardDTO boardDto) {
